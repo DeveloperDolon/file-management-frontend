55fe39c642fbebf057d760cf57d1da9325b2d373
DeveloperDolon <dolonr718@gmail.com>
2026-03-02 12:36:02 +0600

    shadcn ui components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./store/store";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { Toaster as Sonner } from "sonner";
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Sonner />
-    </Provider>
-  );
-}
+const queryClient = new QueryClient();
+
+const App = () => (
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </Provider>
+);
 
 export default App;
